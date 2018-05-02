28193ae1a2047f2e7ee1a691ab77f9e3e838fc49
alex <akourtisdev@gmail.com>
2018-05-02 09:19:41 +0300

    loop to display results

--- src/App.js
+++ src/App.js
@@ -2,70 +2,87 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// variable which holds the endpoint api
-const api = "https://api.chucknorris.io/jokes/random";
+// api url
+const api = "https://api.chucknorris.io/jokes/search?query=";
+let resultsTable; // variable which will containt fetched results in html format to be rendered
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
+      // initialize properties of obejct
       values: "",
       id: ""
     };
   }
 
-  validateInput = () => {
-    let badInput;
-    // check input if false fetchres () else alert message!
-    fetchres();
+  // trims input and allows character and spaces only
+  trimInput = () => {
+    var regex = /[^a-z ]/gi;
+    let input = document.getElementById('searchInput').value.replace(regex, "");
+    console.log("user input is: " + input);
+
+    this.fetchres(input); // after user input is trimmed do fetching
+    input = "";
   }
 
-  fetchres = () => {
-    let output;
-    const req = new Request(api, {
+  // fetches results of query
+  fetchres = input => {
+    let search = api + input;
+    const req = new Request(search, {
       method: 'GET',
       cache: 'default'
     });
+
     fetch(req)
     .then((res) => res.json())
-    .then((data) => {
-      console.dir(data.value)
-      console.dir(data.icon_url) 
-      this.fillTable(data.value, data.icon_url);
-    }).catch(err =>  {
-      console.log("ERROR: "+ err);
-    })
-    
-  }
+    .then(data => {
 
-  fillTable = (val, icon) => {
-    document.getElementById('results').innerHTML = `<table style="width:50%; margin: auto">
-    <tr>
-      <th>Picture</th>
-      <th>Quote</th> 
-    </tr>
-    <tr>
-      <td><img src=${icon}></td>
-      <td>${val}</td> 
-    </tr>
-  </table>` //val + "<br>" + id;
-  }
+      // log obj for debugging
+      console.log(Object.values(data));
+
+      // fill html table with results
+      this.fillTable(data);
+
+        }).catch(err =>  {      
+        console.log("ERROR: "+ err);
+      })
+    };
+
+  // add data to html table
+  fillTable = (data) => {
+    // loop through it
+    var totalJokes = Object.keys(data.result).length;
+    let output;
+    for ( var i = 1; i <= 10; i++){
+        output += 
+        `<table style="width:50%;">
+          <tr>
+            <td style="width:20%";><img src=${data.result[i].icon_url}></td>
+            <td style="width:80%";>${data.result[i].value}</td>
+          </tr>
+          </table>
+        `
+      }
+      resultsTable += document.getElementById('results').innerHTML = output;
+    }
+  
 
   render() {
     return (
       <div className="App">
-          <form name="input" action="" placeholder="Search something">
               <label>Search: </label>
-              <input id="searchInput" type="text"/>
-              <button type="button" onClick={this.validateInput}>Submit</button>
-          </form>
-          <div id="results">
-          
-            It seems that there are no results.
+              <input id="searchInput" placehoder="Search anything" type="text"/>
+              <button type="button" onClick={this.trimInput}>Submit</button>
+              <table id="table">
+              <tr>
+                <th>Icon</th>
+                <th>Quote</th> 
+              </tr></table>
+              <div id="results"></div>
           </div>
-      </div>
+      
     );
   }
 }
-
 export default App;
