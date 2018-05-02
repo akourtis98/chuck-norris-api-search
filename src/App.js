416078b8b7571a26bbdeb595761f3e3675db1cf2
alex <akourtisdev@gmail.com>
2018-05-02 10:26:56 +0300

    load corresponding list and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -45,44 +46,100 @@ class App extends Component {
       this.fillTable(data);
 
         }).catch(err =>  {      
-        console.log("ERROR: "+ err);
+          this.emptyList();
       })
     };
 
   // add data to html table
   fillTable = (data) => {
+    let output = "";
     // loop through it
     var totalJokes = Object.keys(data.result).length;
-    let output;
-    for ( var i = 1; i <= 10; i++){
-        output += 
-        `<table style="width:50%;">
-          <tr>
-            <td style="width:20%";><img src=${data.result[i].icon_url}></td>
-            <td style="width:80%";>${data.result[i].value}</td>
-          </tr>
-          </table>
-        `
-      }
+    if (totalJokes >= 1){
+      for ( var i = 1; i <= 10; i++){
+          output += 
+          `<table style="width:50%;">
+            <tr>
+              <td style="width:20%";><img src=${data.result[i].icon_url}></td>
+              <td style="width:80%";>${data.result[i].value}</td>
+            </tr>
+            </table>
+          `
+        }
+        this.populateList();
+    }else{
+      this.emptyList();
+      console.log("very few");
+    }
       resultsTable += document.getElementById('results').innerHTML = output;
     }
   
-
-  render() {
-    return (
-      <div className="App">
-              <label>Search: </label>
-              <input id="searchInput" placehoder="Search anything" type="text"/>
-              <button type="button" onClick={this.trimInput}>Submit</button>
-              <table id="table">
-              <tr>
-                <th>Icon</th>
-                <th>Quote</th> 
-              </tr></table>
-              <div id="results"></div>
-          </div>
-      
-    );
-  }
+    populateList  = () => {
+      const list = (
+        <div className="App">
+                <label>Search: </label>
+                <input id="searchInput" placehoder="Search anything" type="text"/>
+                <button type="button" onClick={this.trimInput}>Submit</button>
+                <table id="table">
+                <tr>
+                  <th>Icon</th>
+                  <th>Quote</th> 
+                </tr></table>
+                <div id="results"></div>
+            </div>
+      );
+      ReactDOM.render(list, document.getElementById('root'));
+    }
+    
+    populateList  = () => {
+      const resultlist = (
+        <div className="App">
+                <label>Search: </label>
+                <input id="searchInput" placehoder="Search anything" type="text"/>
+                <button type="button" onClick={this.trimInput}>Submit</button>
+                <table id="table">
+                <tr>
+                  <th>Icon</th>
+                  <th>Quote</th> 
+                </tr></table>
+                <div id="results"></div>
+            </div>
+      );
+      ReactDOM.render(resultlist, document.getElementById('root'));
+    }
+    
+    emptyList  = () => {
+      const emptylist = (
+        <div className="App">
+                <label>Search: </label>
+                <input id="searchInput" placehoder="Search anything" type="text"/>
+                <button type="button" onClick={this.trimInput}>Submit</button>
+                <table id="table">
+                <tr>
+                  <th>Icon</th>
+                  <th>Quote</th> 
+                </tr></table>
+                <div>No results for what you searched</div>
+            </div>
+      );
+      ReactDOM.render(emptylist, document.getElementById('root'));
+    }
+    
+    render() {
+      return (
+        <div className="App">
+                <label>Search: </label>
+                <input id="searchInput" placehoder="Search anything" type="text"/>
+                <button type="button" onClick={this.trimInput}>Submit</button>
+                <table id="table">
+                <tr>
+                  <th>Icon</th>
+                  <th>Quote</th> 
+                </tr></table>
+                <div>Search for something</div>
+            </div>
+        
+      );
+    }
 }
 export default App;
