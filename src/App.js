5c4ca34caa5acc894d6c1a2001cdad1580151af0
alex <akourtisdev@gmail.com>
2018-05-02 05:29:53 +0300

    fetch random and display it in table

--- src/App.js
+++ src/App.js
@@ -2,17 +2,67 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// variable which holds the endpoint api
+const api = "https://api.chucknorris.io/jokes/random";
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      values: "",
+      id: ""
+    };
+  }
+
+  validateInput = () => {
+    let badInput;
+    // check input if false fetchres () else alert message!
+    fetchres();
+  }
+
+  fetchres = () => {
+    let output;
+    const req = new Request(api, {
+      method: 'GET',
+      cache: 'default'
+    });
+    fetch(req)
+    .then((res) => res.json())
+    .then((data) => {
+      console.dir(data.value)
+      console.dir(data.icon_url) 
+      this.fillTable(data.value, data.icon_url);
+    }).catch(err =>  {
+      console.log("ERROR: "+ err);
+    })
+    
+  }
+
+  fillTable = (val, icon) => {
+    document.getElementById('results').innerHTML = `<table style="width:50%; margin: auto">
+    <tr>
+      <th>Picture</th>
+      <th>Quote</th> 
+    </tr>
+    <tr>
+      <td><img src=${icon}></td>
+      <td>${val}</td> 
+    </tr>
+  </table>` //val + "<br>" + id;
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <form name="input" action="" placeholder="Search something">
+              <label>Search: </label>
+              <input id="searchInput" type="text"/>
+              <button type="button" onClick={this.validateInput}>Submit</button>
+          </form>
+          <div id="results">
+          
+            It seems that there are no results.
+          </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// variable which holds the endpoint api
+const url = "https://api.chucknorris.io/jokes/search?query=";
+
+ReactDOM.render(<App/>, document.getElementById('root'));
